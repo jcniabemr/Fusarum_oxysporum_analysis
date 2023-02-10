49d63cf8aaa0f9191576cf3ff37a0370f87a90f6
jcniabemr <john.connell@niab.com>
2023-02-10 20:18:28 +0000

    update

---
+++ kallisto.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#SBATCH -J kallisto 
+#SBATCH -p short 
+#SBATCH --mem=20G
+#SBATCH --cpus-per-task=4
+
+#####Script to create kallisto index then pseudoalign and count transcipts 
+
+source /mnt/shared/scratch/jconnell/apps/miniconda3/bin/activate kallisto 
+
+transcriptome=$1
+fread=$2
+rread=$3
+outdir=$4
+fasta=$(basename $transcriptome .fasta)
+
+workdir=/mnt/shared/scratch/jconnell/kallisto_JOB_${SLURM_JOBID}
+mkdir -p $workdir
+cp -r $transcriptome $fread $rread $workdir
+cd $workdir
+
+kallisto index \
+-k 31 \
+-i "$fasta".idx \
+$transcriptome	 
+
+kallisto quant \
+--index "$fasta".idx \
+--output-dir $outdir \
+$fread \
+$rread	
+
+rm -r $workdir	
--- kallisto_counting.sh
+++ kallisto_counting.sh
@@ -4,6 +4,8 @@
 #SBATCH --mem=2G
 #SBATCH --cpus-per-task=4
 
+####Kallisto count 
+
 for strain in $(ls /home/jconnell/fusarium/rna_seq_data/lettuce_alignment | grep "AJ516"); do
 Transcriptome=/home/jconnell/fusarium/rna_seq_data/re_annotation/AJ516_2022/final_annotations/mk2/remove_dup_features/final_genes_appended_renamed.cds.fasta
     for RawData in /home/jconnell/fusarium/rna_seq_data/decontaminated/AJ516/$strain; do
@@ -11,9 +13,10 @@ Transcriptome=/home/jconnell/fusarium/rna_seq_data/re_annotation/AJ516_2022/fina
         FileR=$RawData/R/*cleaned.fq.gz
         echo $FileF
         echo $FileR
-        OutDir=/home/jconnell/fusarium/salmon/$strain
+        OutDir=/home/jconnell/fusarium/kallisto/$strain
         mkdir -p $OutDir
         ProgDir=/home/jconnell/git_repos/niab_repos/fusarium_oxysporum
-        sbatch $ProgDir/salmon.sh $Transcriptome $FileF $FileR $OutDir
+        sbatch $ProgDir/kallisto.sh $Transcriptome $FileF $FileR $OutDir
     done
 done
+
